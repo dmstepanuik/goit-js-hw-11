b919fff19bb7a5c7aa8fa54cc3da527cf54c0179
dmstepanuik <0669218646@ukr.net>
2022-10-04 22:04:57 +0300

    task- 11

--- src/js/index.js
+++ src/js/index.js
@@ -1,20 +1,64 @@
-import axios from 'axios';
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
 import 'notiflix/dist/notiflix-3.2.5.min.css';
-
+import '../css/styles.css';
+import '../css/gallery.css';
+// import '../css/switch-btn.css';
+import { ItemsApi } from './ItemsApi';
 import { getItemMarkup } from './markup/get-item-markup';
 
 const galleryRef = document.querySelector('.gallery');
 const searchFormRef = document.querySelector('.search-form');
+const loadMoreRef = document.querySelector('.load-more');
+
+const itemsApi = new ItemsApi();
 
 searchFormRef.addEventListener('submit', onSearchFormSubmit);
+loadMoreRef.addEventListener('click', onLoadMoreClick);
 
-function onSearchFormSubmit(e) {
+async function onSearchFormSubmit(e) {
   e.preventDefault();
   const input = e.target.elements.searchQuery;
-  console.dir(input.value);
-  renderList([{}, {}]);
+
+  await itemsApi
+    .fetchItems(input.value)
+    .then(onSuccessFetchItems)
+    .catch(e => {
+      removeChildren(galleryRef);
+      Notify.failure(e.message);
+    });
+
+  if (!itemsApi.isLastPage()) {
+    loadMoreRef.classList.remove('is-hidden');
+  } else {
+    loadMoreRef.classList.add('is-hidden');
+  }
 }
+
+function onSuccessFetchItems(result) {
+  removeChildren(galleryRef);
+  renderList(result.data.hits);
+}
+
+async function onLoadMoreClick(e) {
+  e.preventDefault();
+  itemsApi.incrementPage();
+
+  try {
+    const res = await itemsApi.fetchItems();
+    onSuccessFetchItems(res);
+  } catch (e) {
+    Notify.failure(e.message);
+  }
+
+  if (itemsApi.isLastPage()) {
+    loadMoreRef.classList.add('is-hidden');
+    Notify.warning(
+      "We're sorry, but you've reached the end of search results."
+    );
+    itemsApi.resetPage();
+  }
+}
+
 function removeChildren(el) {
   while (el.firstChild) {
     el.removeChild(el.firstChild);
@@ -27,14 +71,4 @@ function renderList(itemList) {
 
 function getItemListMarkup(itemList) {
   return itemList.map(it => getItemMarkup(it)).join('');
-}
-
-// https://pixabay.com/api/?key=30341549-2658c35ea7dbba4c0cdeabc27&q=yellow+flowers&image_type=photo
-
-function fetchItems(string) {
-  axios
-    .get(
-      'https://pixabay.com/api/?key=30341549-2658c35ea7dbba4c0cdeabc27&q=yellow+flowers&image_type=photo'
-    )
-    .then(res => {});
 }
--- src/js/markup/get-item-markup.js
+++ src/js/markup/get-item-markup.js
@@ -1,21 +1,35 @@
-export function getItemMarkUp(data) {
+export function getItemMarkup(data) {
+  const {
+    webformatURL,
+    largeImageURL,
+    tags,
+    likes,
+    views,
+    comments,
+    downloads,
+  } = data;
+
   return `
-    <div class="photo-card">
-      <img src="" alt="" loading="lazy" />
+    <a class="photo-card" href="${largeImageURL}"> 
+      <img class="photo-card__img" src="${webformatURL}" alt="${tags}" loading="lazy" />
       <div class="info">
         <p class="info-item">
           <b>Likes</b>
+          ${likes}
         </p>
         <p class="info-item">
           <b>Views</b>
+          ${views}
         </p>
         <p class="info-item">
           <b>Comments</b>
+          ${comments}
         </p>
         <p class="info-item">
           <b>Downloads</b>
+          ${downloads}
         </p>
       </div>
-    </div>
+    </a>
   `;
 }
